1568a0b60c3bb188352de1c347a82b8a8a8fdde7
DJScully <49269301+DJScully@users.noreply.github.com>
2020-11-05 13:39:13 +0100

    Commit 05/11

--- NTT/JS/eventos.js
+++ NTT/JS/eventos.js
@@ -1,9 +1,9 @@
 document.addEventListener("readystatechange", cargarEventos, false);
-
+var list = 0;
 function cargarEventos(evento) {
 	if(document.readyState == "interactive") {
         document.getElementById("boton").addEventListener("click",openNav,false);
-
+        //document.getElementById("boton").addEventListener("click",closeBtn,false);
         
         var total = document.getElementsByClassName("menu");
         total[0].addEventListener("click",closeNav,true);
@@ -16,102 +16,54 @@ function cargarEventos(evento) {
         total[7].addEventListener("click",closeNav,true);
         total[8].addEventListener("click",closeNav,true);
         total[9].addEventListener("click",closeNav,true);
-
-       
-        
-
         document.getElementsByClassName("nope")[0].addEventListener("click",closeNav,true);
-        document.getElementsByClassName("nope")[1].addEventListener("click",closeNav,true);
-        document.getElementsByClassName("listado")[0].addEventListener("mouseover",more,true);
-        document.getElementsByClassName("listado")[1].addEventListener("mouseover",normal,true);
-        document.getElementsByClassName("listado")[2].addEventListener("mouseover",less,true);
-        document.getElementsByClassName("listado")[3].addEventListener("mouseover",normal,true);
-        document.getElementsByClassName("listado")[4].addEventListener("mouseover",less,true);
-        document.getElementsByClassName("listado")[5].addEventListener("mouseover",normal,true);
-        document.getElementsByClassName("listado")[6].addEventListener("mouseover",normal,true);
-        document.getElementsByClassName("listado")[0].addEventListener("mouseout",less,true);
-        document.getElementsByClassName("listado")[0].addEventListener("click",lesser,true);
-
-       /* for (let index = 0; index < document.getElementsByClassName("listado").length; index++) {
-            document.getElementsByClassName("listado")[index].addEventListener("mouseout",lesser,true);
-            
-        }*/
+        document.getElementsByClassName("nope")[1].addEventListener("click",closeNav,true)
 	}
 }
 //alert(document.getElementsByClassName("sub")[0].style.right);
 
+alert(document.getElementsByClassName("sub").length);
+function res() {
+    alert(window.outerWidth);
+    if ( window.outerWidth >= 1228) {
+        for (let index = 0; index < document.getElementsByClassName("sub").length; index++) {
+            document.getElementsByClassName("sub")[index].style.right= "auto";
+            
+        }
+    }
+}
+
+
 function openNav() {
-    var a = document.getElementsByClassName("fullWidth")[0].offsetWidth;if( a < 1299){
-        document.getElementById("boton").style.marginRight = "30%";
+    var a = document.getElementsByClassName("fullWidth")[0].offsetWidth;if( a < 1228){
         document.getElementById("boton").style.transition = ".3s";
         document.getElementById("lista").style.display = "block";
         document.getElementById("list").style.display = "block";
         document.getElementById("list").style.right = "0";
         document.getElementById("boton").innerHTML = "X"
-        for (let index = 0; index < document.getElementsByClassName("sub").length; index++) {
-            document.getElementsByClassName("sub")[index].style.right= "100%";
-            
-        }
         document.getElementById("boton").removeEventListener("click",openNav);
-        document.getElementById("boton").addEventListener("click", closeBtn, false)
+        document.getElementById("boton").addEventListener("click", closeBtn, true)
     }
 }
 
-function more() {
-    var a = document.getElementsByClassName("fullWidth")[0].offsetWidth;
-    if( a < 1299){
-        document.getElementById("boton").style.marginRight = "62%";
-
-    }
-}
-
-function normal() {
-    var a = document.getElementsByClassName("fullWidth")[0].offsetWidth;
-    if( a < 1299){
-        document.getElementById("boton").style.marginRight = "30%";
-
-    }
-}
-
-function less() {
-    var a = document.getElementsByClassName("fullWidth")[0].offsetWidth;
-    if( a < 1299){
-        document.getElementById("boton").style.marginRight = "30%";
-      
-    }
-    
-    
-}
-
-function lesser() {
-    var a = document.getElementsByClassName("fullWidth")[0].offsetWidth;
-    if( a < 1299){
-        document.getElementById("boton").style.marginRight = "0";
-    }
-    
-
-
-}
 
 function closeNav() {
 
             var a = document.getElementsByClassName("fullWidth")[0].offsetWidth;
-            if( a < 1299 && a > 1000){
-                    document.getElementById("boton").style.marginRight = "0%";
-                    document.getElementById("boton").style.display = "initial";
-                    document.getElementById("lista").style.display = "block";
-                    document.getElementById("list").style.right = "-30%";
-                    document.getElementById("boton").innerHTML = "☰";
-                    document.getElementsByClassName("sub")[0].style.right= "0";
-                    
-            }  else if (a <= 1000) {
-                    document.getElementById("boton").style.marginRight = "0%";
-                    document.getElementById("boton").style.display = "initial";
-                    document.getElementById("lista").style.display = "block";
+            if( a < 1228 && a > 1000){
+
                     document.getElementById("list").style.right = "-30%";
                     document.getElementById("boton").innerHTML = "☰";
                     for (let index = 0; index < document.getElementsByClassName("sub").length; index++) {
-                        document.getElementsByClassName("sub")[index].style.right= "0";
+                        document.getElementsByClassName("sub")[index].style.right= "auto";
+                        
+                    }
+                    
+            }  else if (a <= 1000) {
+                    document.getElementById("list").style.right = "-30%";
+                    document.getElementById("boton").innerHTML = "☰";
+                    for (let index = 0; index < document.getElementsByClassName("sub").length; index++) {
+                        document.getElementsByClassName("sub")[index].style.right= "auto";
                         
                     }
                   
@@ -119,21 +71,20 @@ function closeNav() {
 
 }
 
-//alert(document.getElementById("boton").innerHTML);
+function name() {
+    document.getElementById("boton").innerHTML = "☰";
+}
 function closeBtn() {
 
     var a = document.getElementsByClassName("fullWidth")[0].offsetWidth;
-    if( a < 1299){
-        
-            document.getElementById("boton").style.marginRight = "0";
-            document.getElementById("boton").style.display = "initial";
+    if( a < 1228){
             document.getElementById("lista").style.display = "block";
             document.getElementById("list").style.right = "-30%";
             document.getElementById("boton").innerHTML = "☰";
-           
+      }     
             document.getElementById("boton").removeEventListener("click", closeBtn);
             document.getElementById("boton").addEventListener("click",openNav,false);
-    }
+    
 
 
 }
